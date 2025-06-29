17dff539ea9eae36ba12ed198c01b73a433efc34
Pandit <22it3037@rgipt.ac.in>
2025-06-29 17:31:58 +0530

    utils code discussion

--- complete_Backend_Project/src/index.js
+++ complete_Backend_Project/src/index.js
@@ -8,7 +8,20 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB() //async return a promise
+.then( ()=>{
+    app.on("Error", (error) => {
+        console.log("Error: ", error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection Failed !!! ", error);
+})
 
 
 
