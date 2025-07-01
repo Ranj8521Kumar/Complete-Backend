b570f6397ecac1dbeb43ebd8cf7d37394a4b8a52
Pandit <22it3037@rgipt.ac.in>
2025-07-01 11:30:07 +0530

    add refresh access token functionality and update routes

--- complete_Backend_Project/src/controllers/user.controller.js
+++ complete_Backend_Project/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {ApiError} from "../utils/Apierror.js";
 import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiRespose.js";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try{
@@ -178,8 +179,54 @@ const logOutUser = asyncHandler(async  (req, res) => {
     .json(new ApiResponse(200, "User logged Out"));
 });
 
+const refreshAcessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body;
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "unauthorised request");
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        );
+    
+        const user = await User.findById(decodedToken?._id);
+    
+        if(!user){
+            throw new ApiError(401, "Invalid refresh token");
+        }
+    
+        if(incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401, "refresh token is expired or used");
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id);
+    
+        return res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {accessToken, refreshToken: newRefreshToken},
+                "Acess token refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Error refreshed token");
+    }
+});
+
 export {
     registerUser,
     loginUser,
-    logOutUser
+    logOutUser,
+    refreshAcessToken,
 };
